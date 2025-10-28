732f0ef4f57102b607f9e8715caa147ee7b5a598
Nikita Rybak <fisher.nikita+github@gmail.com>
2025-10-28 20:16:10 +0300

    1SEM-DW-1.1lab

---
+++ 1SEM/DigitalWorkshop/LaboratoryWork1.1.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+unsigned main()
+{
+	unsigned a, b;
+	a = 4294967295;
+	while (a % 71585 != 0) {
+		a = a - 1;
+	}
+	while (a != 71585) {
+		printf("%u ", a);
+		a = a - 71585;
+	}
+	printf("%u", a);
+}
